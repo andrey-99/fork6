d5733b6dadc679017049830b0fa22a50364e31d7
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-05-17 15:41:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,6 @@ struct SYM {
   int prioritet;
 };
 int main() {
-    TPQueue<SYM> pqueue;
-    pqueue.push(SYM{'a', 4});
-    pqueue.push(SYM{'b', 7});
-    SYM c1 = pqueue.pop();
-    SYM c2 = pqueue.pop();
-    return 0;
   TPQueue<SYM> pqueue;
   pqueue.push(SYM{'a', 4});
   pqueue.push(SYM{'b', 7});
