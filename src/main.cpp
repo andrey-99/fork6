2c2c0b1074e601691d5af781a80ef52742ec9d4b
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-05-17 15:58:53 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,24 @@
 #include <iostream>
 #include "tpqueue.h"
 
+struct SYM {
+  char ch;
+  int prior;
+};
+
 int main() {
   TPQueue<SYM> pqueue;
   pqueue.push(SYM{'a', 4});
   pqueue.push(SYM{'b', 7});
   pqueue.push(SYM{'c', 2});
+
   SYM c1 = pqueue.pop();
   SYM c2 = pqueue.pop();
   SYM c3 = pqueue.pop();
-  std::cout << c1.ch << " " << c1.prioritet << '\n';
-  std::cout << c2.ch << " " << c2.prioritet << '\n';
-  std::cout << c3.ch << " " << c3.prioritet << '\n';
+
+  std::cout << c1.ch << " " << c1.prior << '\n';
+  std::cout << c2.ch << " " << c2.prior << '\n';
+  std::cout << c3.ch << " " << c3.prior << '\n';
+
   return 0;
 }
